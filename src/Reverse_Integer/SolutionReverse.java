d34506958326da43d9a8d17c8036779d2d36b003
igor <igor.ramos.c@hotmail.com>
2022-12-29 14:46:45 -0500

    ejercicio 7 aceptado con runtime: 0 ms

---
+++ src/Reverse_Integer/SolutionReverse.java
@@ -0,0 +1,31 @@
+package Reverse_Integer;
+
+public class SolutionReverse {
+    /**
+     * @author Igor Ramos Cruzado
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        boolean negativo = false;
+        if( x < 0 ){
+            x = x*(-1);
+            negativo = true;
+        }
+        int reversoNumero = 0;
+        int auxNumero = x;
+        byte longitud = 0;
+        while(auxNumero > 0){
+            auxNumero/=10;
+            longitud++;
+        }
+        for(int i = 0 ; i < longitud ; i++){
+            reversoNumero = reversoNumero*10 + (x%10);
+            x/=10;
+        }
+        if(negativo == true){
+            reversoNumero = reversoNumero*(-1);
+        }
+        return reversoNumero;
+    }
+}
