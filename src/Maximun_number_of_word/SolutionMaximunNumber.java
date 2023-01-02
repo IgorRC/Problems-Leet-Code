0fda25390dab0274a5ec5f929bf2ec2e023f2573
igor <igor.ramos.c@hotmail.com>
2023-01-02 00:14:10 -0500

    ejercicio 2114 aceptado con runtime 3 ms

---
+++ src/Maximun_number_of_word/SolutionMaximunNumber.java
@@ -0,0 +1,22 @@
+package Maximun_number_of_word;
+
+public class SolutionMaximunNumber {
+    public int mostWordsFound(String[] sentences) {
+        int acumuladorEspacion = 0;
+        int numeroMaximoEspacios = 0;
+        for(int i = 0 ; i < sentences.length ; i++){
+            for (int j = 0 ; j < sentences[i].length() ; j++){
+                if(sentences[i].charAt(j) == ' '){
+                    acumuladorEspacion++;
+                }
+                
+            }
+            if(acumuladorEspacion > numeroMaximoEspacios){
+                numeroMaximoEspacios = acumuladorEspacion;
+            }
+            acumuladorEspacion = 0;
+        }
+        
+        return numeroMaximoEspacios+1;   
+    }
+}
